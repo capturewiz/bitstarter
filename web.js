6c70a11d5a271a37a1c0aabbeddff52c7317c0c8
capturewiz <startup@pixelmetrics.com>
2013-07-11 16:18:53 +0000

    fix index.html

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var fs = require('fs');
-  var rRdBuf = fs.readFileSync(index.html);
+  var rRdBuf = fs.readFileSync('index.html');
   response.send(rRdBuf.toString());
 });
 
