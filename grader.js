55902d76284d6bdbc5bc58c2ffd76ebddc1ebd7c
capturewiz <startup@pixelmetrics.com>
2013-07-12 03:00:19 +0000

    url works!

--- grader.js
+++ grader.js
@@ -24,7 +24,11 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+var util = require('util');
+
 var HTMLFILE_DEFAULT = "index.html";
+var HTMLURL_DEFAULT = "http://obscure-ridge-7482.herokuapp.com";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
@@ -36,23 +40,58 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var assertValidUrl = function(aInUrl) {
+    return aInUrl.toString();
+};
+
+var cheerioHtmlFile = function(aHtmlstring) {
+    return cheerio.load(aHtmlstring);
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
-    var checks = loadChecks(checksfile).sort();
+var checkHtmlFile = function(aHtmlfile, aChecksfile, aHtmlurl) {
+    console.error('aHtmlfile=' + aHtmlfile + ', aChecksfile=' + aChecksfile + ', aHtmlurl=' + aHtmlurl);
+    if (true) {
+	rest.get(aHtmlurl).on(
+	    'complete', 
+	    handleRestResponse(aChecksfile)
+        );
+
+    } else {
+	console.error("Read from local file");
+	handleHtmlString(fs.readFileSync(aHtmlfile), aChecksfile);
+    };
+};
+
+var handleRestResponse = function(aChecksfile) {
+    return function(aResult) {
+	if (aResult instanceof Error) {
+	    console.error('Error: ' + util.format(aResponse.message));
+	    
+	} else {
+	    console.error("Successful download");
+	    handleHtmlString(aResult, aChecksfile);
+	}
+    };
+};
+
+var handleHtmlString = function(aHtmlstring, aChecksfile) {
+    console.error("aHtmlstring=" + aHtmlstring + ", aChecksfile=" + aChecksfile);
+    $ = cheerioHtmlFile(aHtmlstring);
+
+    var checks = loadChecks(aChecksfile).sort();
+
     var out = {};
     for(var ii in checks) {
         var present = $(checks[ii]).length > 0;
         out[checks[ii]] = present;
-    }
-    return out;
+    };
+
+    var outJson = JSON.stringify(out, null, 4);
+    console.log(outJson);
 };
 
 var clone = function(fn) {
@@ -64,11 +103,10 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <html_url>', 'Url to index.html', clone(assertValidUrl), HTMLURL_DEFAULT)
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    checkHtmlFile(program.file, program.checks, program.url);
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
